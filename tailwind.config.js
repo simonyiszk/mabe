3ac0beae02a7a1d4561a19958831bd94029ed89a
Gergő Radeczki <g.radeczki@gmail.com>
2021-09-16 10:50:29 +0200

    Added colors from Figma file

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,19 @@ module.exports = {
 				DEFAULT: defaultTheme.transitionTimingFunction.out,
 			},
 		},
+		colors: { // Figma fájlból importálva
+			turquoise: {
+				light: "#A5FFD6",
+				dark: "#84DCC6",
+			},
+			pink: {
+				light: "#FFA69E",
+				dark: "#FF686B",
+			},
+			gray: {
+				DEFAULT: "#222222",
+			},
+		},
 		margin: (theme, { negative }) => ({
 			auto: "auto",
 			...negative(theme("spacing")),
