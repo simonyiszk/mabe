c73c339e10b8da234e561e26f968f918b59df94c
Dániel Miklós <daniel.miklos0@gmail.com>
2021-09-17 17:36:10 +0200

    temporarily use text as logo

--- src/components/header/Logo.tsx
+++ src/components/header/Logo.tsx
@@ -1,14 +1,5 @@
 import Image from "next/image";
-import Link from "next/link";
-
-import mabelogo from "@/components/header/logo3.png";
 
 export function Logo() {
-	return (
-		<Link href="/">
-			<a className="select-none">
-				<Image src={mabelogo} width={85} height={50} alt="Logo of MABE" />
-			</a>
-		</Link>
-	);
+	return <h1>MABELOGO</h1>;
 }
