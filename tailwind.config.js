191654905ca16767b3df57c80db641d78aac087d
Dániel Miklós <daniel.miklos0@gmail.com>
2021-09-18 14:19:57 +0200

    extend colors properly

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,20 +9,21 @@ module.exports = {
 			transitionTimingFunction: {
 				DEFAULT: defaultTheme.transitionTimingFunction.out,
 			},
+			colors: {
+				turquoise: {
+					light: "#A5FFD6",
+					dark: "#84DCC6",
+				},
+				pink: {
+					light: "#FFA69E",
+					dark: "#FF686B",
+				},
+				gray: {
+					DEFAULT: "#222222",
+				},
+			},
 		},
-		colors: { // Figma fájlból importálva
-			turquoise: {
-				light: "#A5FFD6",
-				dark: "#84DCC6",
-			},
-			pink: {
-				light: "#FFA69E",
-				dark: "#FF686B",
-			},
-			gray: {
-				DEFAULT: "#222222",
-			},
-		},
+
 		margin: (theme, { negative }) => ({
 			auto: "auto",
 			...negative(theme("spacing")),
