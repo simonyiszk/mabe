76210aefbc4254a770832f9013a398be053de1b1
János Fehér <feherjani96@gmail.com>
2021-09-18 14:21:10 +0200

    Fix tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,18 +9,18 @@ module.exports = {
 			transitionTimingFunction: {
 				DEFAULT: defaultTheme.transitionTimingFunction.out,
 			},
-		},
-		colors: { // Figma fájlból importálva
-			turquoise: {
-				light: "#A5FFD6",
-				dark: "#84DCC6",
-			},
-			pink: {
-				light: "#FFA69E",
-				dark: "#FF686B",
-			},
-			gray: {
-				DEFAULT: "#222222",
+			colors: {
+				turquoise: {
+					light: "#A5FFD6",
+					dark: "#84DCC6",
+				},
+				pink: {
+					light: "#FFA69E",
+					dark: "#FF686B",
+				},
+				gray: {
+					DEFAULT: "#222222",
+				},
 			},
 		},
 		margin: (theme, { negative }) => ({
