d12751e0b03631c2d465b9a687a7878eea55bd49
Dániel Miklós <daniel.miklos0@gmail.com>
2021-09-19 12:14:30 +0200

    removeunnecessary clsx usage

--- src/components/header/HamburgerMenu.tsx
+++ src/components/header/HamburgerMenu.tsx
@@ -1,5 +1,3 @@
-import clsx from "clsx";
-
 import { HamburgerButton } from "@/components/header/HamburgerButton";
 import { HeaderLinkList } from "@/components/header/HeaderLinkList";
 
@@ -9,7 +7,7 @@ type HamburgerMenuProps = {
 
 export function HamburgerMenu({ buttonOnClick }: HamburgerMenuProps) {
 	return (
-		<div className={clsx("z-50", "py-4 pl-16 w-full h-screen")}>
+		<div className="z-50 py-4 pl-16 w-full h-screen">
 			<ul className="flex flex-col justify-evenly h-full">
 				<li>
 					<HamburgerButton onClick={buttonOnClick} active />
