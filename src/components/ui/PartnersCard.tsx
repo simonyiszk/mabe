a6f1b9da027f3b7378b701c4c25f5c7056828926
Dániel Miklós <daniel.miklos0@gmail.com>
2021-09-19 23:58:44 +0200

    text and img into divs

--- src/components/ui/PartnersCard.tsx
+++ src/components/ui/PartnersCard.tsx
@@ -6,11 +6,13 @@ import hand from "@/assets/images/hand.svg";
 export function PartnersCard() {
 	return (
 		<Link href="/partnerek" passHref>
-			<div className="py-8 px-4 m-auto max-w-xs sm:max-w-xl rounded-mainPage shadow-mainPageCards">
-				<div className="flex flex-col lg:flex-row lg:items-center space-y-4 lg:space-y-0">
-					<Image src={hand} height={78} />
+			<div className="py-8 px-4 lg:px-16 m-auto max-w-xs sm:max-w-xl rounded-mainPage shadow-mainPageCards cursor-pointer">
+				<div className="flex flex-col lg:flex-row lg:justify-center items-center space-y-4 lg:space-y-0">
 					<div>
-						<h3 className="text-2xl lg:text-3xl font-medium text-center lg:text-left uppercase">
+						<Image src={hand} height={78} />
+					</div>
+					<div className="w-auto">
+						<h3 className="text-2xl lg:text-3xl font-medium text-center lg:text-left hover:text-turquoise-dark uppercase">
 							Partnerekkel való megismerkedés
 						</h3>
 					</div>
