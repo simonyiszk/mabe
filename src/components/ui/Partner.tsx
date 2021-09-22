4596d994165cc907da19d366c280382eab77a3de
Gergő Radeczki <g.radeczki@gmail.com>
2021-09-22 19:00:22 +0200

    removed unused code

--- src/components/ui/Partner.tsx
+++ src/components/ui/Partner.tsx
@@ -15,7 +15,7 @@ export function Partner({ logo, address, email, name, tel, website }: PartnerPro
 	const shortUrl = website.replace(/(^\w+:|^)\/\//, '');
 
 	return (
-		<div className="p-2 w-56 partner">
+		<div className="p-2 w-56">
 			<div className="flex items-center p-4 w-40 h-40 mx-auto relative">
 				<Image
 					layout="fill"
@@ -26,7 +26,7 @@ export function Partner({ logo, address, email, name, tel, website }: PartnerPro
 			</div>
 			<div
 				className="flex-col justify-center p-6 space-y-4 w-52 text-xs text-center rounded-b-lg shadow-figma">
-				<div className="space-y-4 text-xs drop-shadow-none	">
+				<div className="space-y-4 text-xs">
 					<p className="text-base font-bold">{name}</p>
 					<hr className="h-0.5 bg-pink-dark rounded-3xl border-none " />
 					<p>{address}</p>
