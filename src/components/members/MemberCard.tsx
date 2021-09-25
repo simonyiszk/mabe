eb8bdd27adae3d68578323a4b63456cf94dd6d63
Dániel Miklós <daniel.miklos0@gmail.com>
2021-09-26 00:00:32 +0200

    PR #57 comment fix

--- src/components/members/MemberCard.tsx
+++ src/components/members/MemberCard.tsx
@@ -16,7 +16,7 @@ export function MemberCard({
 }: MemberCardProps) {
 	return (
 		<div className="flex flex-row items-center space-x-6 w-full font-roboto rounded-full shadow-memberCard">
-			<div className="relative w-28 xl:w-36 2xl:w-48 h-28 xl:h-36 2xl:h-48">
+			<div className="relative flex-shrink-0 w-28 xl:w-36 2xl:w-48 h-28 xl:h-36 2xl:h-48">
 				<Image
 					src={image_link}
 					layout="fill"
@@ -27,16 +27,16 @@ export function MemberCard({
 				{/* Strange image background effect */}
 				<div className="z-0 ml-1 w-full h-full bg-turquoise-dark rounded-full" />
 			</div>
-			<div className="flex relative flex-col h-full">
-				<h1
+			<div className="flex flex-col self-stretch py-2 xl:py-4 pr-3">
+				<h2
 					className={clsx(
 						name.length > 16 && "text-sm",
 						"text-lg xl:text-3xl font-bold",
 					)}
 				>
 					{name}
-				</h1>
-				<h2 className="xl:text-3xl font-light ">{position}</h2>
+				</h2>
+				<h2 className="flex-1 xl:text-xl font-light ">{position}</h2>
 				<a href={`mailto:${email}`} className="text-sm xl:text-2xl">
 					<p>{email}</p>
 				</a>
