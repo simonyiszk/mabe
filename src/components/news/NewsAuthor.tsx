81471b1f9c7a3b0e47d8006ea5c264e81ab7edfc
Dániel Miklós <daniel.miklos0@gmail.com>
2021-09-25 22:54:13 +0200

    add NewsAuthor panel

---
+++ src/components/news/NewsAuthor.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+
+type NewsAuthorProps = {
+	name: string;
+	description: string;
+	image_link: string;
+};
+
+export function NewsAuthor({ name, description, image_link }: NewsAuthorProps) {
+	return (
+		<div className="flex flex-row items-center pt-8 w-full">
+			<div className="relative w-16 h-16">
+				<Image
+					src={image_link}
+					className="rounded-full"
+					layout="fill"
+					objectFit="cover"
+				/>
+			</div>
+			<div className="px-8">
+				<h1 className="font-bold">{name}</h1>
+				<p>{description}</p>
+			</div>
+		</div>
+	);
+}
