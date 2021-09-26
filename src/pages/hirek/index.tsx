b3dbb3654f8444461f5885cfa586a8fdeae9779d
Dániel Miklós <daniel.miklos0@gmail.com>
2021-09-26 12:16:39 +0200

    not using EventsPageLayout

--- src/pages/hirek/index.tsx
+++ src/pages/hirek/index.tsx
@@ -1,11 +1,11 @@
-import { EventsPageLayout } from "@/components/layouts/EventsPageLayout";
+import { AnyPageLayout } from "@/components/layouts/AnyPageLayout";
 import { NewsAuthor } from "@/components/news/NewsAuthor";
 import { NewsCard } from "@/components/news/NewsCard";
 import { EVENTS } from "@/mock";
 
-export default function EventsPage() {
+export default function NewsPage() {
 	return (
-		<EventsPageLayout>
+		<AnyPageLayout>
 			<>
 				<h1 className="mb-12 font-roboto-slab text-5xl">Hírek</h1>
 				<section className="grid grid-cols-1 xl:grid-cols-2 gap-24">
@@ -28,6 +28,6 @@ export default function EventsPage() {
 					))}
 				</section>
 			</>
-		</EventsPageLayout>
+		</AnyPageLayout>
 	);
 }
