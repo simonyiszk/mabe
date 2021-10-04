1527e54d7a24a669014cbdef31cb70988f537efc
János Fehér <34893944+OmTheTurtle@users.noreply.github.com>
2021-10-04 22:41:15 +0200

    Update member card styles (#77)

--- src/components/members/MemberCard.tsx
+++ src/components/members/MemberCard.tsx
@@ -5,17 +5,15 @@ import type { IMembersFields } from "@/@types/generated/contentful";
 
 export function MemberCard({ name, image, position, email }: IMembersFields) {
 	return (
-		<div className="flex flex-row items-center space-x-6 w-full font-roboto rounded-full shadow-memberCard">
-			<div className="relative flex-shrink-0 w-28 xl:w-36 2xl:w-48 h-28 xl:h-36 2xl:h-48">
+		<div className="flex flex-row items-center space-x-6 w-full rounded-2xl shadow-memberCard">
+			<div className="relative flex-shrink-0 w-28 xl:w-36 2xl:w-48 h-28 xl:h-36 2xl:h-48 border-r-4 border-turquoise-dark">
 				<Image
 					src={`https:${image.fields.file.url}`}
 					layout="fill"
 					alt={name}
-					className="z-10 w-full h-full rounded-full"
+					className="w-full h-full rounded-l-2xl"
 					objectFit="cover"
 				/>
-				{/* Strange image background effect */}
-				<div className="z-0 ml-1 w-full h-full bg-turquoise-dark rounded-full" />
 			</div>
 			<div className="flex flex-col self-stretch py-2 xl:py-4 pr-3">
 				<h2
