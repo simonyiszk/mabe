d2577a7e13051ffe7c1a5c453b320003d16da69e
János Fehér <34893944+OmTheTurtle@users.noreply.github.com>
2021-10-05 15:03:40 +0200

    Fix and update footer (#79)

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -10,21 +10,32 @@ type FooterProps = {
 
 export function Footer({ links }: FooterProps) {
 	return (
-		<footer className="relative z-40 py-2 w-full h-64 text-white bg-gray">
-			<div className="container flex flex-col justify-evenly items-center mx-auto h-full font-bold">
-				<h1 className="text-xl">Kapcsolat</h1>
+		<footer className="flex flex-col items-center py-5 space-y-5 w-full text-white bg-gray">
+			<h1 className="font-roboto-slab text-xl">Kapcsolat</h1>
+			<div className="space-y-3">
 				<ContactWithIcon text="mbioteche@gmail.com" iconType="Mail" />
 				<ContactWithIcon
 					text="1202 Budapest Perczel Mór utca 16."
 					iconType="MapPinPoint"
 				/>
 				<ContactWithIcon text="+36703692392" iconType="Phone" />
-				<FooterSocialIcons
-					FacebookLink={links?.FacebookLink}
-					InstagramLink={links?.InstagramLink}
-					LinkedInLink={links?.LinkedInLink}
-				/>
 			</div>
+			<FooterSocialIcons
+				FacebookLink={links?.FacebookLink}
+				InstagramLink={links?.InstagramLink}
+				LinkedInLink={links?.LinkedInLink}
+			/>
+			<p className="mt-3 font-roboto-slab">
+				designed and made with ❤️ by{" "}
+				<a
+					href="https://schdesign.hu"
+					target="_blank"
+					rel="noopener noreferrer"
+					className="hover:text-pink-dark underline "
+				>
+					schdesign
+				</a>
+			</p>
 		</footer>
 	);
 }
