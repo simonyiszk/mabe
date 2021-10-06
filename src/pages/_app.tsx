71effea19dbda0d60fab6a724e46190defcb420b
Miklos Daniel <46491285+perryd01@users.noreply.github.com>
2021-10-06 11:08:30 +0200

    add analytics code (#83)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,26 @@ import "@/polyfills";
 
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 import { PageLayout } from "@/components/layouts/PageLayout";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+	const router = useRouter();
+
+	useEffect(() => {
+		const handleRouteChange = (url: string) => {
+			// @ts-expect-error google analytics window obj
+			window.gtag("config", process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
+				page_path: url,
+			});
+		};
+		router.events.on("routeChangeComplete", handleRouteChange);
+		return () => {
+			router.events.off("routeChangeComplete", handleRouteChange);
+		};
+	}, [router.events]);
 	return (
 		<>
 			<Head>
