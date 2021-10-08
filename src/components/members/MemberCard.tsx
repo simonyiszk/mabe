33f4e1e7ca4846186362991b39d5e5482e35dacc
János Fehér <feherjani96@gmail.com>
2021-10-08 02:02:20 +0200

    Refactor member card

--- src/components/members/MemberCard.tsx
+++ src/components/members/MemberCard.tsx
@@ -1,11 +1,10 @@
-import clsx from "clsx";
 import Image from "next/image";
 
 import type { IMembersFields } from "@/@types/generated/contentful";
 
 export function MemberCard({ name, image, position, email }: IMembersFields) {
 	return (
-		<div className="flex flex-row items-center space-x-6 w-full rounded-2xl shadow-memberCard">
+		<div className="flex flex-row items-center space-x-3 w-full rounded-2xl shadow-memberCard">
 			<div className="relative flex-shrink-0 w-28 xl:w-36 2xl:w-48 h-28 xl:h-36 2xl:h-48 border-r-4 border-turquoise-dark">
 				<Image
 					src={`https:${image.fields.file.url}`}
@@ -16,16 +15,16 @@ export function MemberCard({ name, image, position, email }: IMembersFields) {
 				/>
 			</div>
 			<div className="flex flex-col self-stretch py-2 xl:py-4 pr-3">
-				<h2
-					className={clsx(
-						name.length > 16 && "text-sm",
-						"text-lg xl:text-3xl font-bold",
-					)}
-				>
+				<h2 className="2xl:mb-3 text-lg xl:text-xl 2xl:text-3xl font-bold">
 					{name}
 				</h2>
-				<h2 className="flex-1 xl:text-xl font-light ">{position}</h2>
-				<a href={`mailto:${email}`} className="text-sm xl:text-2xl">
+				<h2 className="flex-1 text-sm xl:text-base 2xl:text-lg font-light">
+					{position}
+				</h2>
+				<a
+					href={`mailto:${email}`}
+					className="text-sm xl:text-base 2xl:text-lg"
+				>
 					<p>{email}</p>
 				</a>
 			</div>
