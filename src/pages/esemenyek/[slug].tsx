94644ef6caf7c761a56a8f18dee18ae0087c08fd
János Fehér <feherjani96@gmail.com>
2021-10-08 02:20:09 +0200

    Add title

--- src/pages/esemenyek/[slug].tsx
+++ src/pages/esemenyek/[slug].tsx
@@ -1,6 +1,7 @@
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 import { ChevronLeftIcon } from "@heroicons/react/solid";
 import type { GetStaticProps } from "next";
+import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -16,45 +17,50 @@ export default function EventsPage({
 }) {
 	return (
 		<EventsPageLayout>
-			<article>
-				<div className="relative w-full h-52">
-					<Image
-						src={
-							image
-								? `https:${image.fields.file.url}`
-								: "https://placekitten.com/500/500"
-						}
-						className="w-full"
-						layout="fill"
-						objectFit="cover"
-					/>
-				</div>
-				<div className="flex flex-col pb-6 mt-14">
-					<div className="flex flex-col md:flex-row lg:flex-col 2xl:flex-row md:justify-between 2xl:justify-between md:items-center lg:items-start 2xl:items-center mb-5">
-						<h2 className="mb-3 md:mb-0 lg:mb-3 xl:mb-0 text-4xl lg:text-6xl font-medium">
-							{title}
-						</h2>
-						{startDate && endDate && (
-							<div className="min-w-[30%] text-2xl xl:text-3xl font-normal">
-								{new Date(startDate).toLocaleDateString("hu")} -{" "}
-								{new Date(endDate).toLocaleDateString("hu")}
-							</div>
-						)}
+			<>
+				<Head>
+					<title key="title">{title}</title>
+				</Head>
+				<article>
+					<div className="relative w-full h-52">
+						<Image
+							src={
+								image
+									? `https:${image.fields.file.url}`
+									: "https://placekitten.com/500/500"
+							}
+							className="w-full"
+							layout="fill"
+							objectFit="cover"
+						/>
 					</div>
-					<div className="mb-10 text-2xl lg:text-4xl font-bold text-turquoise-dark">
-						{location}
+					<div className="flex flex-col pb-6 mt-14">
+						<div className="flex flex-col md:flex-row lg:flex-col 2xl:flex-row md:justify-between 2xl:justify-between md:items-center lg:items-start 2xl:items-center mb-5">
+							<h2 className="mb-3 md:mb-0 lg:mb-3 xl:mb-0 text-4xl lg:text-6xl font-medium">
+								{title}
+							</h2>
+							{startDate && endDate && (
+								<div className="min-w-[30%] text-2xl xl:text-3xl font-normal">
+									{new Date(startDate).toLocaleDateString("hu")} -{" "}
+									{new Date(endDate).toLocaleDateString("hu")}
+								</div>
+							)}
+						</div>
+						<div className="mb-10 text-2xl lg:text-4xl font-bold text-turquoise-dark">
+							{location}
+						</div>
+						<div className="mb-10 max-w-none prose lg:prose-xl">
+							{documentToReactComponents(longContent, renderOptions)}
+						</div>
+						<Link href="/esemenyek">
+							<a className="flex items-center self-start pr-2 text-turquoise-dark hover:text-white hover:bg-turquoise-dark rounded-full border-2 border-turquoise-dark">
+								<ChevronLeftIcon className="w-6 h-6" />
+								<span className="ml-0.5 text-sm font-bold">Vissza</span>
+							</a>
+						</Link>
 					</div>
-					<div className="mb-10 max-w-none prose lg:prose-xl">
-						{documentToReactComponents(longContent, renderOptions)}
-					</div>
-					<Link href="/esemenyek">
-						<a className="flex items-center self-start pr-2 text-turquoise-dark hover:text-white hover:bg-turquoise-dark rounded-full border-2 border-turquoise-dark">
-							<ChevronLeftIcon className="w-6 h-6" />
-							<span className="ml-0.5 text-sm font-bold">Vissza</span>
-						</a>
-					</Link>
-				</div>
-			</article>
+				</article>
+			</>
 		</EventsPageLayout>
 	);
 }
