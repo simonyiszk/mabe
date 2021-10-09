6df0dce7a2879cd8612e86caa50b5ac40175a8a5
János Fehér <34893944+OmTheTurtle@users.noreply.github.com>
2021-10-09 12:09:11 +0200

    Add priority attribute to SVG images (#111)

--- src/components/homepage/PartnersCard.tsx
+++ src/components/homepage/PartnersCard.tsx
@@ -9,7 +9,7 @@ export function PartnersCard() {
 			<div className="relative z-30 py-8 px-4 lg:px-16 mx-auto max-w-xs sm:max-w-2xl bg-white rounded-mainPage shadow-mainPageCards cursor-pointer">
 				<div className="flex flex-col lg:flex-row lg:justify-center items-center space-y-4 lg:space-y-0">
 					<div className="flex-shrink-0">
-						<Image src={hand} height={78} />
+						<Image src={hand} height={78} priority />
 					</div>
 					<div className="w-auto">
 						<h3 className="font-roboto-slab text-2xl lg:text-3xl font-medium text-center lg:text-left hover:text-turquoise-dark uppercase">
