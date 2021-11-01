9322f0d76363f9e9db8dba4878baa4814170f7bc
Dániel Miklós <daniel.miklos0@gmail.com>
2021-11-01 23:52:16 +0100

    base document

---
+++ src/pages/dokumentumok/index.tsx
@@ -0,0 +1,14 @@
+import { AnyPageLayout } from "@/components/layouts/AnyPageLayout";
+
+export default function DocumentsPage() {
+	return (
+		<AnyPageLayout>
+			<>
+				<h1 className="mb-12 font-roboto-slab text-5xl">Dokumentumok</h1>
+				<section className="grid md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-20 place-items-center">
+					asd
+				</section>
+			</>
+		</AnyPageLayout>
+	);
+}
