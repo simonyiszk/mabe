1f800c29415a6c91d4b43cfbe3e35706ed10f121
Dániel Miklós <daniel.miklos0@gmail.com>
2021-11-03 11:53:20 +0100

    render only if has elements

--- src/pages/dokumentumok/index.tsx
+++ src/pages/dokumentumok/index.tsx
@@ -25,16 +25,17 @@ export default function DocumentsPage({
 			<>
 				<h1 className="mb-12 font-roboto-slab text-5xl">Dokumentumok</h1>
 
-				{y.map((e) => (
-					<div className="mb-4">
-						<h3 className="my-4 text-2xl font-bold">{e}</h3>
-						<section className="grid grid-cols-1 lg:grid-cols-3 gap-8">
-							{documents.items.map(({ fields }) => (
-								<DocumentCard key={fields.name} {...fields} />
-							))}
-						</section>
-					</div>
-				))}
+				{y.length >= 0 &&
+					y.map((e) => (
+						<div className="mb-4">
+							<h3 className="my-4 text-2xl font-bold">{e}</h3>
+							<section className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+								{documents?.items.map(({ fields }) => (
+									<DocumentCard key={fields.name} {...fields} />
+								))}
+							</section>
+						</div>
+					))}
 			</>
 		</AnyPageLayout>
 	);
