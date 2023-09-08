810adafd0efe683e2369ae1d6c2c3058228dbefe
Miklos Daniel <46491285+perryd01@users.noreply.github.com>
2023-09-08 16:04:41 +0200

    reorder menu links (#159)

--- src/components/header/HeaderLinkList.tsx
+++ src/components/header/HeaderLinkList.tsx
@@ -5,11 +5,11 @@ export function HeaderLinkList() {
 		<>
 			<HeaderLink text="Kezdőlap" href="/" />
 			<HeaderLink text="Blog" href="/blog" />
-			<HeaderLink text="Csapatunk" href="/csapatunk" />
 			<HeaderLink text="Események" href="/esemenyek" />
-			<HeaderLink text="Partnerek" href="/partnerek" />
+			<HeaderLink text="Csapatunk" href="/csapatunk" />
 			<HeaderLink text="Galéria" href="/galeria" />
 			<HeaderLink text="Dokumentumok" href="/dokumentumok" />
+			<HeaderLink text="Partnerek" href="/partnerek" />
 			<HeaderLink text="Csatlakozz" specialChar="+" href="/csatlakozz" />
 		</>
 	);
