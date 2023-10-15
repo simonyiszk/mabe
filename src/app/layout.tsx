8ad5d8edd00c3628a721ae4db0caa56a7b3d9bdc
Miklos Daniel <46491285+perryd01@users.noreply.github.com>
2023-10-15 20:11:16 +0200

    New footer (#163)
    * install react icons
    
    * upload team logos
    
    * refactor footer
    
    * fetch social links in footer
    
    * remove parameter
    
    * delete unused layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,13 +44,7 @@ export default function RootLayout({
 				<div className="flex min-h-screen flex-col font-roboto">
 					<Header />
 					<main className="flex-1">{children}</main>
-					<Footer
-						links={{
-							FacebookLink: "https://www.facebook.com/mbioteche",
-							InstagramLink: "https://instagram.com/mbioteche",
-							LinkedInLink: "https://mbioteche.hu/hamarosan",
-						}}
-					/>
+					<Footer />
 				</div>
 			</body>
 		</html>
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,42 +1,85 @@
-import { ContactWithIcon } from "@/components/footer/ContactWithIcon";
-import {
-	FooterSocialIcons,
-	type FooterSocialIconsProps,
-} from "@/components/footer/FooterSocialIcons";
+import Image from "next/image";
+import Link from "next/link";
+import { FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa";
 
-type FooterProps = {
-	links?: FooterSocialIconsProps;
-};
+import { getGeneralData } from "@/utils/contentful";
 
 // eslint-disable-next-line import/prefer-default-export
-export function Footer({ links }: FooterProps) {
+export async function Footer() {
+	const data = await getGeneralData();
+	const { mabeFacebook, mabeInstagram, mabeLinkedIn } = data.fields;
 	return (
-		<footer className="flex w-full flex-col items-center space-y-5 bg-gray py-5 text-white">
-			<h1 className="font-roboto-slab text-xl">Kapcsolat</h1>
-			<div className="space-y-3">
-				<ContactWithIcon text="mbioteche@gmail.com" iconType="Mail" />
-				<ContactWithIcon
-					text="1202 Budapest Perczel Mór utca 16."
-					iconType="MapPinPoint"
-				/>
-				<ContactWithIcon text="+36703692392" iconType="Phone" />
+		<footer className="flex  w-full flex-col items-center space-y-5 bg-gray py-16  text-white">
+			<div className="container grid w-full grid-cols-1 gap-8 px-4">
+				<div className="flex flex-row justify-center md:justify-start">
+					<div className="relative flex h-16 w-64 justify-center">
+						<Image
+							src="/mabe-text.svg"
+							alt="mabe mininmal logo"
+							fill
+							className="h-full w-full"
+						/>
+					</div>
+				</div>
+
+				<ul className="flex flex-row flex-wrap justify-center gap-2 font-roboto-slab text-lg md:justify-start">
+					<li>
+						<Link href="/blog">Blog</Link>
+					</li>
+					<li>
+						<Link href="/esemenyek">Események</Link>
+					</li>
+					<li>
+						<Link href="/csapatunk">Események</Link>
+					</li>
+					<li>
+						<Link href="/galeria">Galéria</Link>
+					</li>
+					<li>
+						<Link href="/dokumentumok">Dokumentumok</Link>
+					</li>
+					<li>
+						<Link href="/partnerek">Partnerek</Link>
+					</li>
+				</ul>
+
+				<h2 className="text-center font-roboto-slab text-2xl font-medium md:text-left lg:text-4xl">
+					Ha érdekel a MaBE tevékenysége, és szeretnél részt venni
+					programjainkon,{" "}
+					<Link
+						scroll
+						href="/csatlakozz"
+						className=" text-turquoise-dark underline"
+					>
+						csatlakozz
+					</Link>{" "}
+					hozzánk!
+				</h2>
+
+				<div className="flex flex-col items-center justify-between gap-4 md:flex-row">
+					<div className="flex flex-col items-center md:items-start">
+						<span className="font-roboto text-sm font-medium">
+							designed and developed by
+						</span>
+						<a href="https://schdesign.hu" target="_blank" rel="noreferrer">
+							<div className="relative h-8 w-32">
+								<Image src="/schdesign-white.svg" alt="schdesign logo" fill />
+							</div>
+						</a>
+					</div>
+					<div className="flex flex-row gap-2 text-4xl">
+						<a href={mabeFacebook} target="_blank" rel="noreferrer">
+							<FaFacebook />
+						</a>
+						<a href={mabeInstagram} target="_blank" rel="noreferrer">
+							<FaInstagram />
+						</a>
+						<a href={mabeLinkedIn} target="_blank" rel="noreferrer">
+							<FaLinkedin />
+						</a>
+					</div>
+				</div>
 			</div>
-			<FooterSocialIcons
-				FacebookLink={links?.FacebookLink}
-				InstagramLink={links?.InstagramLink}
-				LinkedInLink={links?.LinkedInLink}
-			/>
-			<p className="mt-3 font-roboto-slab">
-				designed and made with ❤️ by{" "}
-				<a
-					href="https://schdesign.hu"
-					target="_blank"
-					rel="noopener noreferrer"
-					className="underline hover:text-pink-dark "
-				>
-					schdesign
-				</a>
-			</p>
 		</footer>
 	);
 }
